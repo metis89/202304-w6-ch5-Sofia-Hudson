c32182019a62c3ac4d757e5093e28b2cb88e06e7
Sofia Hudson <sofia-sama@hotmail.com>
2023-06-11 16:54:47 +0200

    Add app and repo

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import createDebug from 'debug';
+const debug = createDebug('W6:App');
+
+export const app = express();
+
+debug('Loaded Express App');
+
+const corsOptions = {
+  origin: '*',
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.use(morgan('dev'));
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use('/route', sampleRouter);
---
+++ src/repository/data.repository.ts
@@ -0,0 +1,54 @@
+import fs from 'fs/promises';
+import createDebug from 'debug';
+const debug = createDebug('W6:SampleRepo');
+
+type Subject = {
+  id: string;
+  name: string;
+};
+
+const file= './data.json';
+
+class DataRepo {
+  constructor(), {
+    debug('Data Repo');
+  }
+
+  async readAll() {
+    const stringData = await fs.readFile(file, { encoding: 'utf-8' });
+    return JSON.parse(stringData) as Subject[];
+  }
+
+  async readById(id: string) {
+    const stringData = await fs.readFile(file, { encoding: 'utf-8' });
+    const subjectData = JSON.parse(stringData) as Subject[];
+    return subjectData.find((item) => item.id === id);
+  }
+
+  async addSubject(subject: Subject) {
+    const stringData = await fs.readFile(file, { encoding: 'utf-8' });
+    const subjectData = JSON.parse(stringData) as Subject[];
+    const newSubjectList = JSON.stringify([...subjectData, subject]);
+    await fs.writeFile(file, newSubjectList, {
+      encoding: 'utf-8',
+    });
+  }
+
+  async update(id: string, newSubject: Subject) {
+    const stringData = await fs.readFile(file, { encoding: 'utf-8' });
+    const subjectData = JSON.parse(stringData) as Subject[];
+    const updatedList = subjectData.map((item) =>
+      item.id === id ? { ...item, ...newSubject } : item
+    );
+    const updatedFile = JSON.stringify(updatedList);
+    await fs.writeFile(file, updatedFile, { encoding: 'utf-8' });
+  }
+
+  async delete(id: string) {
+    const stringData = await fs.readFile(file, { encoding: 'utf-8' });
+    const subjectData = JSON.parse(stringData) as Subject[];
+    const updatedList = subjectData.filter((item) => item.id !== id);
+    const updatedFile = JSON.stringify(updatedList);
+    await fs.writeFile(file, updatedFile, { encoding: 'utf-8' });
+  }
+}
